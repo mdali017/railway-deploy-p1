d07c3412b9971fd4a352ee84821fdcc2b8ba4343
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-11-12 12:38:33 +0600

    port define

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the server.");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port : ${port}`);
 });
