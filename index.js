bf20a14b6be097248eef6147182e69d63a917cbf
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-11-12 12:54:20 +0600

    create product api

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const port = process.env.PORT || 4000;
 // middleware
 app.use(cors());
 
-// testing
+// root
 app.get("/", (req, res) => {
   res.send("Welcome to the server.");
+});
+
+// testing
+app.get("/all-product", (req, res) => {
+  res.send("All Product is here...");
 });
 
 app.listen(port, "0.0.0.0", () => {
